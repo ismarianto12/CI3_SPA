00b1517b8775ef80f91805dbe4d59f1ea696fccd
ismarianto12 <kotokareh@gmail.com>
2022-07-04 17:15:11 +0700

    adsadad

---
+++ application/views/footer.php
@@ -0,0 +1,17 @@
+ 	<!-- Bootstrap core JavaScript
+    ================================================== -->
+ 	<!-- Placed at the end of the document so the pages load faster -->
+ 	<script src="assets/js/jquery-3.3.1.min.js"></script>
+ 	<script src="assets/js/bootstrap.min.js"></script>
+ 	<script src="assets/js/jquery.pjax.js"></script>
+ 	<script src="assets/js/scripts.js"></script>
+
+ 	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css">
+ 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.1/css/jquery.dataTables.css">
+ 	<script type="text/javascript" src="//cdn.datatables.net/1.10.1/js/jquery.dataTables.min.js"></script>
+ 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/responsive/1.0.0/css/dataTables.responsive.css">
+ 	<script type="text/javascript" src="//cdn.datatables.net/responsive/1.0.0/js/dataTables.responsive.js"></script>
+
+ 	</body>
+
+ 	</html>
